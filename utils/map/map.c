fee00b07337761003537492bb11bd87cb2219510
mkZ3R0 <bscs22021@itu.edu.pk>
2024-10-18 16:56:19 +0500

    removed fail logs

--- utils/map/map.c
+++ utils/map/map.c
@@ -6,53 +6,19 @@ map* createMap(int maxSize)
 {
     // Allocate memory for the map struct
     map *m = (map *)malloc(sizeof(map));
-    if (!m) {
-        printf("Memory allocation failed for map struct.\n");
-        return NULL;
-    }
-
     m->size = 0;
 	m->maxSize = maxSize;
 
     m->keys = (unsigned char **)malloc(maxSize * sizeof(unsigned char *));
-    if (!m->keys) 
-	{
-        printf("Memory allocation failed for keys.\n");
-        free(m);
-        return NULL;
-    }
 
     for (int i = 0; i < maxSize; i++) 
 	{
         m->keys[i] = (unsigned char *)malloc(shaLength * sizeof(unsigned char));
-        if (!m->keys[i]) 
-		{
-            printf("Memory allocation failed for keys[%d].\n", i);
-            for (int j = 0; j < i; j++) 
-			{
-				free(m->keys[j]);
-			}
-            free(m->keys);
-            free(m);
-            return NULL;
-        }
         memset(m->keys[i], 0, shaLength);  // Initialize the memory to 0
     }
 
     // Allocate memory for values array
     m->values = (int *)malloc(maxSize * sizeof(int));
-    if (!m->values) 
-	{
-        printf("Memory allocation failed for values.\n");
-        for (int i = 0; i < maxSize; i++) 
-		{
-			free(m->keys[i]);
-		}
-        free(m->keys);
-        free(m);
-        return NULL;
-    }
-
     memset(m->values, 0, maxSize * sizeof(int)); 
 
     return m;
